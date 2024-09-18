133bba92c349e56f10c484b5e5e0d6250a28665c
subharthi75 <subharthi75@gmail.com>
2024-09-18 10:13:33 +0530

    hehe

--- my-isl/pages/api/auth/signin.ts
+++ my-isl/pages/api/auth/signin.ts
@@ -1,38 +1,38 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '@/lib/prisma'; 
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+// import { NextApiRequest, NextApiResponse } from 'next';
+// import prisma from '@/lib/prisma'; 
+// import bcrypt from 'bcrypt';
+// import jwt from 'jsonwebtoken';
 
-export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    const { email, password } = req.body;
+// export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+//   if (req.method === 'POST') {
+//     const { email, password } = req.body;
 
-    if (!process.env.JWT_SECRET) {
-      return res.status(500).json({ message: 'JWT_SECRET environment variable is not set' });
-    }
+//     if (!process.env.JWT_SECRET) {
+//       return res.status(500).json({ message: 'JWT_SECRET environment variable is not set' });
+//     }
 
-    try {
-      const user = await prisma.user.findUnique({ where: { email } });
+//     try {
+//       const user = await prisma.user.findUnique({ where: { email } });
 
-      if (!user) {
-        return res.status(400).json({ message: 'Invalid email or password' });
-      }
+//       if (!user) {
+//         return res.status(400).json({ message: 'Invalid email or password' });
+//       }
 
-      const passwordMatch = await bcrypt.compare(password, user.password);
+//       const passwordMatch = await bcrypt.compare(password, user.password);
 
-      if (!passwordMatch) {
-        return res.status(400).json({ message: 'Invalid email or password' });
-      }
+//       if (!passwordMatch) {
+//         return res.status(400).json({ message: 'Invalid email or password' });
+//       }
 
-      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+//       const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-      res.status(200).json({ message: 'Logged in successfully', token, user: { email: user.email } });
-    } catch (error) {
-      const errorMessage = (error as Error).message || 'An unexpected error occurred';
-      res.status(500).json({ message: errorMessage });
-    }
-  } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-}
+//       res.status(200).json({ message: 'Logged in successfully', token, user: { email: user.email } });
+//     } catch (error) {
+//       const errorMessage = (error as Error).message || 'An unexpected error occurred';
+//       res.status(500).json({ message: errorMessage });
+//     }
+//   } else {
+//     res.setHeader('Allow', ['POST']);
+//     res.status(405).end(`Method ${req.method} Not Allowed`);
+//   }
+// }
