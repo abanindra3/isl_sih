ea5332c07dac9fa8c5dba0056f0c1ae8d6a1d942
subharthi75 <subharthi75@gmail.com>
2024-09-18 10:04:23 +0530

    fixes awrn

--- my-isl/components/auth-components.tsx
+++ my-isl/components/auth-components.tsx
@@ -21,7 +21,7 @@ export function SignUpForm() {
     setSuccess('');
 
     try {
-      const response = null;
+      const response:any = null;
       // await fetch('/api/auth/signup', {
       //   method: 'POST',
       //   headers: { 'Content-Type': 'application/json' },
@@ -94,7 +94,7 @@ export function SignInForm() {
     setError('');
 
     try {
-      const response  = null; 
+      const response:any  = null; 
       // await fetch('/api/auth/signin', {
       //   method: 'POST',
       //   headers: { 'Content-Type': 'application/json' },
